b491aa0f55b9a2271c87d43a2e32799ee7e687ed
Rajat Vadiraj Dwaraknath <rajatvd@gmail.com>
2018-06-08 00:41:38 +0530

    Finished up train loop util, with an example

--- train_utils.py
+++ train_utils.py
@@ -9,8 +9,23 @@ import torch.nn as nn
 from torch import optim
 from pylab import *
 from torch.utils.data import *
-from IPython import display
-import os
+from IPython.display import *
+from nb_figure import *
+import os, time
 
 style.use(['dark_background'])
 rcParams['axes.grid']=True
+rcParams['font.size']=15
+rcParams['figure.figsize']=18,7
+
+def getTimeName():
+    """Return the current time in format <day>-<month>_<hour><minute> for use in filenames."""
+    from datetime import datetime
+    t = datetime.now()
+    return "{:02d}-{:02d}_{:02d}{:02d}".format(t.day,t.month,t.hour,t.minute)
+
+def decorate_plot(axes):
+    ax = axes[0][0]
+    ax.set_title("Training loss")
+    ax.set_xlabel("Epoch")
+    ax.set_ylabel("Loss")
