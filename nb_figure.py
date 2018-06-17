e3df1410a64c357ac006af53d9c0da0c402e3ad0
Rajat Vadiraj Dwaraknath <rajatvd@gmail.com>
2018-06-17 21:54:45 +0530

    Refactored train loop, now supports callback, multiple metrics

--- nb_figure.py
+++ nb_figure.py
@@ -10,11 +10,13 @@ class NBFigure():
     """An extension of matplotlib figure to work with jupyter notebook's display module.
     Works by storing the figure as an image on disk and loading it to display on updating.
     """
-    def __init__(self,image_path,nrows=1,ncols=1,decorate_fn=None,**subplot_kwargs):
+    def __init__(self,image_path=None,nrows=1,ncols=1,decorate_fn=None,**subplot_kwargs):
         """
         image_path: the filename of the stored figure image
-        
-        Rest of the arguments are indentical to subplots' arguments
+        decorate_fn: A function which takes in a 2d array of axes, and should perform
+            basic plot decorations like labeling, title etc. It is called once.
+            
+        subplot_kwargs are passed to subplots' kwargs
         """
         
         self.nrows=nrows
@@ -30,12 +32,22 @@ class NBFigure():
         
         
         self.image_path = image_path
-        self.fig.savefig(image_path, bbox_inches='tight')
+        
+        if self.image_path != None:
+            self.fig.savefig(image_path, bbox_inches='tight')
+            
         self.disp = None
         
         self.xlims = [[(None,None) for j in range(ncols)] for i in range(nrows)]
         self.ylims = [[(None,None) for j in range(ncols)] for i in range(nrows)]
+     
+    def set_image_path(self, image_path):
+        """Sets the path for saving the figure image. This path must be set(either in init 
+        or using this function) before display can be called."""
         
+        self.image_path = image_path        
+        self.fig.savefig(image_path, bbox_inches='tight')
+    
     def update_lims(self):
         """
         Updates lims of all axes
@@ -51,20 +63,22 @@ class NBFigure():
                 
     def set_xlim(self,xlim,axis_num=0):
         """Set the xlims of the axes indexed row major wise starting from 0.
-        Effect will be visible only after update."""
+        Effect will be visible only after update. 
+        Setting either limit to None will make that limit update automatically."""
         row = axis_num//self.ncols
         col = axis_num%self.ncols
         self.xlims[row][col]=xlim
 
     def set_ylim(self,ylim,axis_num=0):
         """Set the ylims of the axes indexed row major wise, starting from 0.
-        Effect will be visible only after update."""
+        Effect will be visible only after update.
+        Setting either limit to None will make that limit update automatically."""
         row = axis_num//self.ncols
         col = axis_num%self.ncols
         self.ylims[row][col]=ylim
         
     def display(self):
-        """Create a new display of the figure"""
+        """Create a display of the figure"""
         self.disp = display(Image(self.image_path),display_id=str(id(self)))
     
     def update(self, update_lims=True):
